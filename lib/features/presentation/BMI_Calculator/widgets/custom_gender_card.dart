39dfd3bb54afe0bc033effc0ca2b1b2f31454b74
DESKTOP-URHPD6C\Shawali <m.atta6470@gmail.com>
2025-06-12 07:07:33 +0300

    finish Custom Card Gender

---
+++ lib/features/presentation/BMI_Calculator/widgets/custom_gender_card.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class CustomGenderCard extends StatelessWidget {
+  const CustomGenderCard({
+    super.key,
+    required this.text,
+    this.onTap,
+    required this.icon,
+    required this.isGender,
+  });
+
+  final String text;
+  final void Function()? onTap;
+  final IconData icon;
+  final bool isGender;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            color: isGender ? Color(0xffF50D56) : Color(0xff111327),
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          width: double.infinity,
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+              Icon(icon, size: 120, color: Colors.white),
+              Text(
+                text,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
